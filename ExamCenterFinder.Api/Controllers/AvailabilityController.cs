208c7b7928afba3746b6a5d223d9dbed1dd02982
FM_Suhail <demonbruster@gmail.com>
2023-12-20 10:02:58 +0530

    feat: Availability controller init

---
+++ ExamCenterFinder.Api/Controllers/AvailabilityController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExamCenterFinder.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AvailabilityController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAvailability([FromQuery] int examDuration, [FromQuery] string zipCode, [FromQuery] int distance)
+        {
+            return Ok();
+        }
+    }
+}
