9c22f1d8e7d350c1c17620c8a2a43a3b358693f3
FM_Suhail <demonbruster@gmail.com>
2023-12-20 10:25:18 +0530

    feat: bad request validation

--- ExamCenterFinder.Api/Controllers/AvailabilityController.cs
+++ ExamCenterFinder.Api/Controllers/AvailabilityController.cs
@@ -6,10 +6,41 @@ namespace ExamCenterFinder.Api.Controllers
     [Route("[controller]")]
     public class AvailabilityController : ControllerBase
     {
+        private readonly ILogger<AvailabilityController> _logger;
+        public AvailabilityController(ILogger<AvailabilityController> logger ) 
+        {
+            _logger = logger;
+        }
         [HttpGet]
         public async Task<IActionResult> GetAvailability([FromQuery] int examDuration, [FromQuery] string zipCode, [FromQuery] int distance)
         {
-            return Ok();
+            try
+            {
+                var validationError = ValidateInputParameters(examDuration, zipCode, distance);
+                if (validationError != null)
+                    return BadRequest(validationError);
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"An error occurred: {ex.Message}");
+                return StatusCode(500, "An error occurred while processing the request.");
+            }
+        }
+
+        private string ValidateInputParameters(int examDuration, string zipCode, int distance)
+        {
+            if (examDuration <= 0)
+                return "Invalid input parameter: examDuration must be a positive value.";
+
+            if (string.IsNullOrEmpty(zipCode))
+                return "Invalid input parameter: zipCode cannot be null or empty.";
+
+            if (distance <= 0)
+                return "Invalid input parameter: distance must be a positive value.";
+
+            return null;
         }
     }
 }
